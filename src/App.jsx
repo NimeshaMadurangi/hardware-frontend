23a8937932a8461581d969f3db3c46c7ba0420cc
IT21178986-Nimesha Madurangi <116791096+NimeshaMadurangi@users.noreply.github.com>
2025-02-26 20:34:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "../src/Components/dashboardComponent/dashboard"
+
+function App() {
+  
+
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
